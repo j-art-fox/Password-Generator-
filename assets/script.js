c4e693fbbf7070db0bb5d79040a164792496d55e
jordanrings <jordanringsakabe@gmail.com>
2022-06-29 16:55:37 -0600

    Instead of using clickable interface, I'm only going to use prompts.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", function () {
+  var promptName = prompt("Please input your desired password length. Must between 8-128 characters.")
+  });
+
+  if(promptName < 8 && promptName > 128 ){
+    alert("Please enter a value of at least 8 and at most 128.");
+  }
+
+
+var Start = document.querySelector("#criteria");
+criteria.addEventListener("click", () => {
+  var element = document.querySelector(".password-criteria");
+  element.style.visibility = "visible";
+});
+
+function generatePassword(){
+ 
+  return "Password will go here.";
+}
+
 
 // Write password to the #password input
 function writePassword() {
@@ -11,4 +31,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword){
+//     console.log(writePassword);
+// };
